19ece421c7656fbfe57f536c9181aef4868c8ef7
santisfdez <sasanchezfe@unal.edu.co>
2022-05-18 00:25:59 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -163,4 +163,5 @@ def pregunta_04():
     print("R^2: {:6.4f}".format(linearRegression.score(X_test, y_test)))
     rmse = np.sqrt(mean_squared_error(X_test, y_test))
     print("Root Mean Squared Error: {:6.4f}".format(rmse))
+    
 
